302df8b0ab36b5d4cac163dfa8f3177599461fcd
João V <thornewwriter@gmail.com>
2019-05-10 18:56:44 -0400

    moving the resources and shader folder to the engine project

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -4,14 +4,12 @@ Sandbox::Sandbox()
 {
 }
 
-
 Sandbox::~Sandbox()
 {
 }
 
 // TODO: Move this main function to the Engine in a entry_point.hpp file
 int main() {
-	Sandbox* sandbox = new Sandbox();
+	auto sandbox = std::unique_ptr<Sandbox>(new Sandbox());
 	sandbox->Run();
-	delete sandbox;
 }
